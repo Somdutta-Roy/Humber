d71467d4d6a613773ae7308d25b80608a146249a
Somdutta Roy <sansomstar84@gmail.com>
2020-11-19 16:39:48 -0500

    Inheritance

---
+++ MainClass.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            DerivedClass DC = new DerivedClass();
+            DC.FirstName = "Som";
+            DC.LastName = "Roy";
+            DC.GetName();
+
+        }
+    }
+
+    public class MySealedClass
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public MySealedClass() { Console.WriteLine("I m Base Class!!!!"); }
+
+        public virtual string GetName()
+        {
+            return string.Format("{0}, {1}", FirstName, LastName);
+        }
+    }
+
+    public class DerivedClass : MySealedClass
+    {
+        public override sealed string GetName()
+        {
+            Console.WriteLine("I m Child's Method!!!!");
+            return string.Format("{0}, {1}", FirstName, LastName);
+        }
+    }
+
+    public class NextDerivedClass : MySealedClass
+    {
+        public override string GetName()
+        {
+            return "";
+        }
+    }
+}
